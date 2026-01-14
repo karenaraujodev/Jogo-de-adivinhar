eaeb253b63210dedc45fca4c5fc46546a2e5e976
Karen Araujo <barbiekarenalves@outlook.com>
2026-01-14 12:55:26 -0300

    criando projeto

---
+++ script.js
@@ -0,0 +1,17 @@
+let numeroSecreto = Math.floor(Math.random() * 10) + 1;
+
+btn.addEventListener("click", function () {
+  const palpite = Number(document.querySelector("#palpite").value);
+
+  if (palpite < 1 || palpite > 10) {
+    resultado.textContent = "Digite um número entre 1 e 10!";
+    return;
+  }
+
+  if (palpite === numeroSecreto) {
+    resultado.textContent = "🎉 Parabéns! Você acertou!";
+    numeroSecreto = Math.floor(Math.random() * 10) + 1;
+  } else {
+    resultado.textContent = "❌ Errou! Tente novamente.";
+  }
+});

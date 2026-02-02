c80523b7e68431f70c0120f0fcc789b3e06430fc
Karen Araujo <barbiekarenalves@outlook.com>
2026-02-02 18:02:26 -0300

    atualização do projeto

--- script.js
+++ script.js
@@ -1,17 +1,70 @@
-let numeroSecreto = Math.floor(Math.random() * 10) + 1;
 
-btn.addEventListener("click", function () {
-  const palpite = Number(document.querySelector("#palpite").value);
+      let randomNumber = Math.floor(Math.random()) + 1;
 
-  if (palpite < 1 || palpite > 10) {
-    resultado.textContent = "Digite um número entre 1 e 10!";
-    return;
-  }
+      const guesses = document.querySelector(".guesses");
+      const lastResult = document.querySelector(".lastResult");
+      const lowOrHi = document.querySelector(".lowOrHi");
+      const guessSubmit = document.querySelector(".guessSubmit");
+      const guessField = document.querySelector(".guessField");
 
-  if (palpite === numeroSecreto) {
-    resultado.textContent = "🎉 Parabéns! Você acertou!";
-    numeroSecreto = Math.floor(Math.random() * 10) + 1;
-  } else {
-    resultado.textContent = "Errou! Tente novamente.";
-  }
-});
+      let guessCount = 1;
+      let resetButton;
+
+      function checkGuess() {
+        const userGuess = Number(guessField.value);
+        if (guessCount === 1) {
+          guesses.textContent = "Palpites anteriores: ";
+        }
+        guesses.textContent += userGuess + " ";
+
+        if (userGuess === randomNumber) {
+          lastResult.textContent = "Parabéns! Você acertou!";
+          lastResult.style.backgroundColor = "green";
+          lowOrHi.textContent = "";
+          setGameOver();
+        } else if (guessCount === 10) {
+          lastResult.textContent = "!!! FIM DE JOGO !!!";
+          setGameOver();
+        } else {
+          lastResult.textContent = "Errado!";
+          lastResult.style.backgroundColor = "red";
+          if (userGuess < randomNumber) {
+            lowOrHi.textContent = "O último palpite foi muito baixo!";
+          } else if (userGuess > randomNumber) {
+            lowOrHi.textContent = "O último palpite foi muito alto!";
+          }
+        }
+
+        guessCount++;
+        guessField.value = "";
+        guessField.focus();
+      }
+      guessSubmit.addEventListener("click", checkGuess);
+
+      function setGameOver() {
+        guessField.disabled = true;
+        guessSubmit.disabled = true;
+        resetButton = document.createElement("button");
+        resetButton.textContent = "Iniciar novo jogo";
+        document.body.appendChild(resetButton);
+        resetButton.addEventListener("click", resetGame);
+      }
+
+      function resetGame() {
+        guessCount = 1;
+
+        const resetParas = document.querySelectorAll(".resultParas p");
+        for (const resetPara of resetParas) {
+          resetPara.textContent = "";
+        }
+        resetButton.parentNode.removeChild(resetButton);
+
+        guessField.disabled = false;
+        guessSubmit.disabled = false;
+        guessField.value = "";
+        guessField.focus();
+
+        lastResult.style.backgroundColor = "white";
+
+        randomNumber = Math.floor(Math.random()) + 1;
+      }
